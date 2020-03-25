db906c41865b0ab8ae41046749614a1c3b572efa
Shuduo Sang <sangshuduo@gmail.com>
2020-03-25 13:44:40 +0800

    add new import taosSql to call CGO module

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module cgo-i-test
 
 go 1.13
 
-require github.com/mattn/go-sqlite3 v2.0.3+incompatible
+require (
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/taosdata/driver-go v0.0.0-20200311072652-8c58c512b6ac
+)
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/taosdata/driver-go/taosSql"
 )
 
 func main() {
 	fmt.Println("This is an example GO project uses CGO, go module, and some popular CI services and unit test frameworks.")
+
+	fmt.Println("add new import taosSql to call CGO module")
 }
